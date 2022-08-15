72b1d1f9b71908de9a5a4f8670e7315a7a611455
rald <thecoconutnutisagiantnut@gmail.com>
2022-08-15 12:54:29 +0800

    OK

--- lyrics.js
+++ lyrics.js
@@ -2,11 +2,13 @@ lyrics = {
 
   "0000":{ 
     title:"Happy Birthday", 
-    text:"Happy birthday to you\nHappy birthday to you\nHappy birthday, Happy birthday\nHappy birthday to you." 
+    author:"Folk Song",
+    text:"Happy birthday to you\nHappy birthday to you\nHappy birthday\nHappy birthday\nHappy birthday to you." 
   },
   
   "0001":{ 
     title:"Ako Ay May Lobo", 
+    author:"Folk Song",
     text:"Ako ay may lobo lumipad sa langit\nDi ko nanakita\npumutok na pala\nsayang lang ang pera ko\nbiniling lobo\nkung sa pagkain sana\nnabusog pa ako." 
   },
 
